763607ee15a1545848cf28633edb8ecc4137712f
palamagoo <abrahamandeggslincoln@gmail.com>
2022-09-21 16:59:41 -0700

    9/21 Main
    -reworked layout + layout plans
    -readded card bidding
    -character selection gui but havent added anything yet

--- portfolio-game/Assets/Scripts/Card.cs
+++ portfolio-game/Assets/Scripts/Card.cs
@@ -8,5 +8,11 @@ public class Card : ScriptableObject
     public string cardName;
     public string cardDesc;
     public int timeAffect;
+    public int bidValue;
+    public int cardEffect;
+    //can write different effects to correspond to different numbers.
+    //for example, cardEffect would = 1 if we want it to shuffle the opponent's cards.
+    //It would = 2 if we want it to shuffle all of your own cards. etc
+    //for bid cards, keep this value 0
     public Sprite image;
 }
